a82755e4fa12366d5ceca56b1d705ee269ebe987
Paul Hempel <paul.hempel@mailbox.tu-dresden.de>
2024-07-30 13:32:32 +0200

    Fix test image generation, change test images to solid color

--- src/blend.cpp
+++ src/blend.cpp
@@ -1,3 +1,5 @@
+#include <array>
+#include <cstdint>
 #include <iostream>
 #include <limits>
 
@@ -17,49 +19,60 @@ auto main(int const argc, char const* const argv[]) -> int try {
 		}
 
 	// Define pixel format.
-	using Pixel   = png::rgba_pixel;
-	using Channel = png::pixel_traits<Pixel>::component_type;
+	using Channel   = uint8_t;
+	using Pixel     = png::basic_rgba_pixel<Channel>;
+	using PixelData = std::array<Channel, Pixel::traits::channels>;
+	using Image     = png::image<Pixel>;
+
+	auto constexpr alpha_channel {Pixel::traits::channels - 1};
+	auto constexpr channel_max   {std::numeric_limits<Channel>::max()};
 
 	// Initialize the output image to the first layer.
-	png::image<Pixel> blend  {argv[1]};
-	auto const        width  {blend.get_width()};
-	auto const        height {blend.get_height()};
+	Image      blend  {argv[1]};
+	auto const width  {blend.get_width()};
+	auto const height {blend.get_height()};
+
+	using ImageSize = std::remove_cv_t<decltype(width)>;
+
+	// Scale colors by opacity.
+	for (ImageSize y {0}; y < height; ++y) {
+		for (ImageSize x {0}; x < width; ++x) {
+			auto& pixel {reinterpret_cast<PixelData&>(blend[y][x])};
+
+			for (auto i {0}; i < alpha_channel; ++i) {
+				pixel[i] = pixel[i] * pixel[alpha_channel] / channel_max;
+				}}}
 
 	// Add remaining layers behind.
 	for (auto const* arg_it {&argv[2]}; arg_it < &argv[argc]; ++arg_it) {
 		// Read image.
-		png::image<Pixel> layer {*arg_it};
+		Image const layer {*arg_it};
 
 		// Ensure all layers have the same size.
 		if (layer.get_width() != width or layer.get_height() != height) {
-			std::cerr << log_sev_warn << "Skipping " << *arg_it
+			std::cerr << log_sev_error << "Skipping " << *arg_it
 			          << ", which has a different size than the first layer.";
 			continue;
 			}
 
 		// Blend pixels using the over operator.
-		for (auto y {0}; y < height; ++y) {
-			for (auto x {0}; x < blend.get_width(); ++x) {
-				auto&       blend_pixel {blend[y][x]};
-				auto const& layer_pixel {layer[y][x]};
+		for (ImageSize y {0}; y < height; ++y) {
+			for (ImageSize x {0}; x < width; ++x) {
+				auto& blend_pixel {reinterpret_cast<PixelData&      >(blend[y][x])};
+				auto& layer_pixel {reinterpret_cast<PixelData const&>(layer[y][x])};
 
-				auto*       blend_channel_it {reinterpret_cast<Channel*      >(&blend_pixel)};
-				auto const* layer_channel_it {reinterpret_cast<Channel const*>(&blend_pixel)};
-
-				auto constexpr channel_max  {std::numeric_limits<Channel>::max()};
-				auto const     transparency {channel_max - blend_pixel.alpha};
+				auto const transparency {channel_max - blend_pixel[alpha_channel]};
 
 				// Blend color channels scaled by opacity.
-				while (blend_channel_it != &blend_pixel.alpha) {
-					*blend_channel_it += (transparency * *layer_channel_it * blend_pixel.alpha)
-					                     / channel_max;
-
-					++blend_channel_it;
-					++layer_channel_it;
+				for (auto i {0}; i < alpha_channel; ++i) {
+					blend_pixel[i] += layer_pixel[i]
+					                  * layer_pixel[alpha_channel] / channel_max
+					                  * transparency               / channel_max;
 					}
 
-				// Blend alpha channels.
-				*blend_channel_it += (transparency * *layer_channel_it) / channel_max;
+				// Blend alpha channel.
+				blend_pixel[alpha_channel] += layer_pixel[alpha_channel]
+				                              * transparency / channel_max;
 				}}}
 
 	// Write final image to stdout.
